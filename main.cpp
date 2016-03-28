226de659ab0e1d72b6ec7da44963e2cebcb80aae
Raphael Bost <raphael_bost@alumni.brown.edu>
2016-03-28 14:26:00 +0200

    Licensed under GPLv3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 //
-//  main.cpp
-//  ssdmap
+// ssdmap - Implementation of a disk-resident map designed for SSDs.
+// Copyright (C) 2016 Raphael Bost
 //
-//  Created by Raphael Bost on 17/03/2016.
-//  Copyright © 2016 Raphael Bost. All rights reserved.
+// This file is part of ssdmap.
 //
+// ssdmap is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ssdmap is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ssdmap.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 #include <iostream>
 #include <fstream>
